07888fd8b2bb3da8bc1636ed4739d255c10eda48
Diandre Miller <millerdiandre@gmail.com>
2023-03-04 17:36:22 -0500

    Added components and updated app.js with components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import React from 'react';
+import Home from './components/pages/Home';
+import Cart from './components/pages/Cart';
+import CheckOut from './components/pages/CheckOut';
+import ExclusiveForm from './components/pages/ExclusiveForm';
 
 function App() {
   return (
     <div className="App">
+      <Home />
+      <Cart />
+      <CheckOut />
+      <ExclusiveForm />
       <p>Hello World</p>
     </div>
   );
